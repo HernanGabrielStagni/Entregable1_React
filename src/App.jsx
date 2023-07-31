88599d4e4412faace11e569fe0081766bf0ff8e2
HernanGabrielStagni <psicologohernanstagni@gmail.com>
2023-07-31 01:32:33 -0300

    ajuste el idioma

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Phrases from './components/Phrases'/*importo componente*/
 import frases from'./utils/frases.json' /*importo json*/
 import getRandomArray from './utils/getRandomArray'
 import { useState } from 'react'
-
+import Lenguage from './components/Lenguage'
 const images = [1,2,3,4,5,6,7,8]
 
 console.log(getRandomArray(frases))
@@ -14,22 +14,36 @@ console.log(getRandomArray(frases))
 function App() {
  const [phraseRandom, setPhraseRandom] = useState(getRandomArray(frases))
  const [numImages, setNumImages] = useState(getRandomArray(images))
+const [lenguageEs, setLenguageEs] = useState(false)
 
  const appStyles={
   backgroundImage:`url('/fortuna-1/fondo${numImages}.jpg')`,
  
  };
-
+// console.log(lenguageEs)
  getRandomArray(frases)
 
   return (
     <div style={appStyles} className="App">
-      <h1 className='App__title'>Galletas de la Fortuna</h1>
+      <h1 className='App__title'>Leyes de Murphy</h1>
+     
      
       <Button 
       setPhraseRandom={setPhraseRandom}
-      setNumImages={setNumImages}/>
-       <Phrases phraseRandom={phraseRandom}/>
+      setNumImages={setNumImages}
+      />
+      
+      <Lenguage
+      setPhraseRandom={setPhraseRandom}
+      setNumImages={setNumImages}
+      setLenguageEs={setLenguageEs}
+      lenguageEs={lenguageEs}
+       /> 
+       
+       <Phrases 
+       phraseRandom={phraseRandom}
+       lenguageEs={lenguageEs}
+       />
      
     </div>
   )
--- src/components/Phrases.jsx
+++ src/components/Phrases.jsx
@@ -1,9 +1,9 @@
 
-const Phrases = ({phraseRandom}) => {
+const Phrases = ({phraseRandom,lenguageEs}) => {
   return (
     <>
     <div className="App__div-phrase">
-        <p className="App__phrase">{phraseRandom.phrase}</p>
+        <p className="App__phrase">{lenguageEs ?phraseRandom.frase :phraseRandom.phrase}</p>
         
     </div>
     <div className="App__div-author">
