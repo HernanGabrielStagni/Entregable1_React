9c7ebad932bf73c5689de8888c39dc0a5a4a01d5
HernanGabrielStagni <psicologohernanstagni@gmail.com>
2023-08-03 03:13:58 -0300

    retire la carpeta public del path

--- src/App.jsx
+++ src/App.jsx
@@ -25,13 +25,13 @@ const [lenguageEs, setLenguageEs] = useState(false)
 
   return (
     <div style={appStyles} className="App">
-      <img className="obj1" src="/public/icons/Sin título-1.png" alt="" />
-      <img className="obj2" src="/public/icons/Sin título-3.png" alt="" />
-      <img className="obj3" src="/public/icons/Sin título-2.png" alt="" />
-      <img className="obj4" src="/public/icons/Sin título-3.png" alt="" /> 
-      <img className="obj2" src="/public/icons/Sin título-2.png" alt="" />
-      <img className="obj2" src="/public/icons/Sin título-3.png" alt="" />
-      <img className="obj3" src="/public/icons/Sin título-1.png" alt="" />
+      <img className="obj1" src="/icons/Sin título-1.png" alt="" />
+      <img className="obj2" src="/icons/Sin título-3.png" alt="" />
+      <img className="obj3" src="/icons/Sin título-2.png" alt="" />
+      <img className="obj4" src="/icons/Sin título-3.png" alt="" /> 
+      <img className="obj2" src="/icons/Sin título-2.png" alt="" />
+      <img className="obj2" src="/icons/Sin título-3.png" alt="" />
+      <img className="obj3" src="/icons/Sin título-1.png" alt="" />
          {/* <span className="obj1">Obj_1</span>
          <span className="obj2">Obj_2</span> 
          <span className="obj3">Obj_3</span>
